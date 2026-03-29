4b26bd1e5ca322846444136b3d8d9716642d3b7a
flyeverywhere <157803330+flyeverywhere@users.noreply.github.com>
2026-03-29 03:08:40 +0000

    fly.java

---
+++ fly.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String course = request.getParameter("course");
+
+        out.println("<html><body>");
+        out.println("<h2>Registration Successful</h2>");
+        out.println("<p>Name: " + name + "</p>");
+        out.println("<p>Email: " + email + "</p>");
+        out.println("<p>Course: " + course + "</p>");
+        out.println("</body></html>");
+    }
+}
